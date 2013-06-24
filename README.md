2b960cddecd5fed676b28532229f0cb7a384693d
Tudip Technologies <tushar_apshankar@tudip.com>
2013-06-25 00:57:12 +0530

    Ecorepay-Magento pilot commit with all required files and steps to integrate it with Magento project.

--- README.md
+++ README.md
@@ -2,3 +2,12 @@ ecorepay-magento
 ================
 
 This repository introduces Ecorepay payment gateway module.
+
+Following are the step to integrate this module with your Magento project:
+	1. Download and extract ZIP of the repository or export after taking clone of the repository
+	2. Copy/Move "App" folder into your Magento project
+	3. Set your merchant's API Key and Auth token on the line no. 127 and 128 of PaymentMethod.php(\app\code\local\Tudip\Ecorepay\Model\PaymentMethod.php)
+	4. Goto "Admin panel -> System -> Configuration -> Sales -> Payment Methods -> Ecorepay" and set Enables to "YES"
+	4. Thats it! Enjoy Ecorepay on Magento.
+	
+Please provide your feedback.
---
+++ app/code/local/Tudip/Ecorepay/Model/PaymentMethod.php
@@ -0,0 +1,185 @@
+<?php
+
+/**
+* Our test CC module adapter
+*/
+class Tudip_Ecorepay_Model_PaymentMethod extends Mage_Payment_Model_Method_Cc
+{
+    /**
+    * unique internal payment method identifier
+    *
+    * @var string [a-z0-9_]
+    */
+    protected $_code = 'ecorepay';
+
+       /**
+     * Here are examples of flags that will determine functionality availability
+     * of this module to be used by frontend and backend.
+     *
+     * @see all flags and their defaults in Mage_Payment_Model_Method_Abstract
+     *
+     * It is possible to have a custom dynamic logic by overloading
+     * public function can* for each flag respectively
+     */
+
+    /**
+     * Is this payment method a gateway (online auth/charge) ?
+     */
+    protected $_isGateway               = true;
+
+    /**
+     * Can authorize online?
+     */
+    protected $_canAuthorize            = true;
+
+    /**
+     * Can capture funds online?
+     */
+    protected $_canCapture              = true;
+
+    /**
+     * Can capture partial amounts online?
+     */
+    protected $_canCapturePartial       = false;
+
+    /**
+     * Can refund online?
+     */
+    protected $_canRefund               = false;
+
+    /**
+     * Can void transactions online?
+     */
+    protected $_canVoid                 = true;
+
+    /**
+     * Can use this payment method in administration panel?
+     */
+    protected $_canUseInternal          = true;
+
+    /**
+     * Can show this payment method as an option on checkout payment page?
+     */
+    protected $_canUseCheckout          = true;
+
+    /**
+     * Is this payment method suitable for multi-shipping checkout?
+     */
+    protected $_canUseForMultishipping  = true;
+
+    /**
+     * Can save credit card information for future processing?
+     */
+    protected $_canSaveCc = false;
+
+    /**
+     * Here you will need to implement authorize, capture and void public methods
+     *
+     * @see examples of transaction specific public methods such as
+     * authorize, capture and void in Mage_Paygate_Model_Authorizenet
+     */
+
+
+	public function authorize(Varien_Object $payment, $amount)
+    {
+    	$order = $payment->getOrder();
+		try {
+    		$billingaddress = $order->getBillingAddress();
+			ob_start();
+			$regionModel = Mage::getModel('directory/region')->load($billingaddress->getData('region_id'));
+			$dob = Mage::getModel('customer/customer')->load($order->getCustomerId())->getDob();
+			$devMode = false;
+			$ipAddress = $_SERVER['REMOTE_ADDR'];
+			if($devMode){
+				$ipAddress = "192.168.1.1";
+			}
+			$dobStr = "";
+			if(!isset($dob)){
+				$dob = $order->getCustomerDob();
+			}
+			if(isset($dob)){
+				$dobStr = str_replace("00:00:00", "", $dob);
+				$dobStr = str_replace("-", "", $dobStr);
+				$dobStr = trim($dobStr);
+			}
+
+			$totals = number_format($amount, 2, '.', '');
+			$fields = array(
+                'Reference'=> $order->getId(),
+                'Amount'=> $totals,
+                'Currency'=> $order->getBaseCurrencyCode(),
+                'Email'=> $billingaddress->getData('email'),
+                'IPAddress'=> $_SERVER['REMOTE_ADDR'],
+                'Phone'=> $billingaddress->getData('telephone'),
+                'FirstName'=> $billingaddress->getData('firstname'),
+                'LastName'=> $billingaddress->getData('lastname'),
+                'DOB'=> $dobStr,
+                'Address'=> $billingaddress->getData('street'),
+                'City'=> $billingaddress->getData('city'),
+                'State'=> $regionModel->getCode(),
+                'PostCode'=> $billingaddress->getData('postcode'),
+                'Country'=> $billingaddress->getData('country_id'),
+                'CardNumber'=> $payment->getCcNumber(),
+                'CardExpMonth'=> $payment->getCcExpMonth(),
+                'CardExpYear'=> $payment->getCcExpYear(),
+                'CardCVV'=> $payment->getCcCid()
+			);
+			$accountId = 'Enter Your Account Id or API User Key';
+			$accountAuth = 'Enter Your Account Auth or API Password Key';
+			$fields_string="<?xml version=\"1.0\" encoding=\"UTF-8\"?><Request type=\"AuthorizeCapture\"><AccountID>".$accountId."</AccountID><AccountAuth>".$accountAuth."</AccountAuth><Transaction>";
+			foreach($fields as $key=>$value) {
+				$fields_string .= '<'.$key.'>'.$value.'</'.$key.'>';
+			}
+			$fields_string .= '</Transaction></Request>';
+			//open connection
+			$ch = curl_init('https://gateway.ecorepay.cc/');
+			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+			curl_setopt($ch, CURLOPT_POST,1);
+			curl_setopt($ch, CURLOPT_POSTFIELDS,$fields_string);
+			curl_setopt($ch, CURLOPT_HEADER ,0); // DO NOT RETURN HTTP HEADERS
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER ,1); // RETURN THE CONTENTS OF THE CALL
+			curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 120); // Timeout on connect (2 minutes)
+			//execute post
+			$data = curl_exec($ch); //This value is the string returned from the bank...
+
+            if (!$data) {
+                throw new Exception(curl_error($ch));
+            }
+			$httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+            if ($httpcode && substr($httpcode, 0, 2) != "20") { //Unsuccessful post request...
+                throw new Exception("Returned HTTP CODE: " . $httpcode . " for this URL: " . $urlToPost);
+            }
+            curl_close($ch);
+        } catch (Exception $e) {
+            $payment->setStatus(self::STATUS_ERROR);
+            $payment->setAmount($amount);
+            $payment->setLastTransId($orderId);
+            $this->setStore($payment->getOrder()->getStoreId());
+            Mage::throwException($e->getMessage());
+        }
+        	$xmlResponse = new SimpleXmlElement($data); //Simple way to parse xml, Magento might have an equivalent class
+			$contents = ob_get_contents();
+			ob_end_clean();
+			error_log($contents);
+            $isPaymentAccepted = $xmlResponse->ResponseCode[0] == 100;
+        if ($isPaymentAccepted) {
+            $this->setStore($payment->getOrder()->getStoreId());
+            $payment->setStatus(self::STATUS_APPROVED);
+            $payment->setAmount($amount);
+            $payment->setLastTransId($orderId);
+        } else {
+            $payment->setStatus(self::STATUS_ERROR);
+            $payment->setAmount($amount);
+            $payment->setLastTransId($orderId);
+            $this->setStore($payment->getOrder()->getStoreId());
+            Mage::throwException("Please check your credit card information.");
+        }
+        return $this;
+
+
+    }
+
+
+}
+
+?>
